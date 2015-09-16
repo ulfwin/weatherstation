f224721ec95f923fc3b3e10c2c74f30c9b3a0cb8
Ulf Winberg <ulfwin@gmail.com>
2015-09-16 23:15:08 +0200

    Changed client script to also receive wind speed data

--- client_script/ble2openhab.py
+++ client_script/ble2openhab.py
@@ -4,7 +4,12 @@ import sys
 import datetime
 
 gatttool_cmd = 'gatttool -b DD:4F:3B:2C:A0:5B -I'
-send2OH = 'curl --header "Content-Type: text/plain" --request PUT --data "%s" http://localhost:8080/rest/items/Battery_Level/state'
+send2OH = 'curl --header "Content-Type: text/plain" --request PUT --data "%s" http://localhost:8080/rest/items/%s/state'
+
+param = [['Battery_Level', '0x2a19'],
+	['Wind_Speed', '0x2a70']]
+
+period = 10
 
 # Start gatttool
 con = pexpect.spawn(gatttool_cmd)
@@ -39,19 +44,22 @@ while 1:
 		connected = 1
 		measNr = 0
 		while connected == 1:
-			con.sendline('char-read-uuid 0x2a19')
-			con.expect(['value:.*\n', pexpect.TIMEOUT], timeout=60)
-			if con.after != pexpect.TIMEOUT: 
-				batt_lvl = eval("0x" + con.after.split()[1])
-				measNr += 1
-				sys.stdout.write("%s Battery level: %i %% (measurement nr %i)\r" %(str(datetime.datetime.now()), batt_lvl, measNr))
-				sys.stdout.flush()
-				pexpect.run(send2OH %str(batt_lvl))
-				time.sleep(60)
-			else:
-				sys.stdout.write("\n")
-				connected = 0
-				con.sendline('disconnect')
-				con.expect('\[   \].*>', timeout=1)
+			for i in range(len(param)):
+				con.sendline('char-read-uuid %s' %param[i][1])
+				con.expect(['value:.*\n', pexpect.TIMEOUT], timeout=60)
+				if con.after != pexpect.TIMEOUT: 
+					value = eval("0x" + ''.join(con.after.split()[1:]))
+					if param[i][0] == 'Wind_Speed':
+						value /= 2000
+					measNr += 1
+					sys.stdout.write("%s %s: %i (measurement nr %i)\n" %(str(datetime.datetime.now()), param[i][0], value, measNr))
+					sys.stdout.flush()
+					pexpect.run(send2OH %(str(value),param[i][0]))
+				else:
+					sys.stdout.write("\n")
+					connected = 0
+					con.sendline('disconnect')
+					con.expect('\[   \].*>', timeout=1)
+			time.sleep(period)
 	else:
 		raise Exception("Unknown gatttool response: %s" %con.after)
