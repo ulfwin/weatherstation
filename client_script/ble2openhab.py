5c6d7801437a993ba2a310236a00b2786b218922
Ulf Winberg <ulfwin@gmail.com>
2017-03-19 21:28:34 +0100

    First implementation of bluepy. Tested and working

--- client_script/ble2openhab.py
+++ client_script/ble2openhab.py
@@ -1,11 +1,28 @@
-from bluepy.btle import Peripheral
+from bluepy import btle
 
-class NotificationDelegate(DefaultDelegate):
+class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
-        DefaultDelegate.__init__(self)
+        btle.DefaultDelegate.__init__(self)
 
-    def handleNotification(self, cHandle, data)
-        print("A notification was received")
+    def handleNotification(self, cHandle, data):
+        print("A notification was received: ", data, "<-- data")
+	print(cHandle)
 
 
-device = Peripheral("f2:5c:dc:a2:94:ea", btle.ADDR_TYPE_RANDOM)
+p = btle.Peripheral("f2:5c:dc:a2:94:ea", btle.ADDR_TYPE_RANDOM)
+p.setDelegate( MyDelegate() )
+
+# Setup to turn notifications on, e.g.
+svc = p.getServiceByUUID( "e198000258b346e7b9a6bedfad46833c" )
+ch = svc.getCharacteristics()[0]
+print(ch.valHandle)
+
+p.writeCharacteristic(ch.valHandle+1, "\x02\x00")
+
+while True:
+    if p.waitForNotifications(600):
+        # handleNotification() was called
+        continue
+
+    print("Waited for 10 min without notification...")
+    # Perhaps do something else here
