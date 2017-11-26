fe6a4daa5a6674fd5329ec729092d5d118a4a6a2
Ulf Winberg <ulfwin@gmail.com>
2017-11-26 18:27:34 +0000

    Adjusted client script to simply poll the battery value

--- client_script/ble2openhab.py
+++ client_script/ble2openhab.py
@@ -1,14 +1,24 @@
 from bluepy import btle
 import struct
 import pexpect
+import datetime
+import time
 
 send2OH = 'curl --header "Content-Type: text/plain" --request PUT --data "%s" http://192.168.0.6:8080/rest/items/%s/state'
+# Timeout in minutes
+timeout = 1.0/6
 
 def listAll(p):
 	for svc in p.getServices():
 		print(svc.uuid.getCommonName())
 		for ch in svc.getCharacteristics():
 			print(" " + str(ch.valHandle) + ": " + ch.uuid.getCommonName())
+def getBatLvl(p):
+	svc = p.getServiceByUUID( "180F" )
+	chBat = svc.getCharacteristics()[0]
+	batVal = struct.unpack('b', chBat.read())[0]
+	pexpect.run(send2OH %(str(batVal), "Battery_Level"))
+	print(batVal)
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
@@ -19,12 +29,7 @@ class MyDelegate(btle.DefaultDelegate):
 	#print(cHandle)
 
 	# Get battery level
-	svc = p.getServiceByUUID( "180F" )
-	chBat = svc.getCharacteristics()[0]
-	batVal = struct.unpack('b', chBat.read())[0]
-	pexpect.run(send2OH %(str(batVal), "Battery_Level"))
-	print(batVal)
-
+        getBatLvl(self)
 
 
 p = btle.Peripheral("f2:5c:dc:a2:94:ea", btle.ADDR_TYPE_RANDOM)
@@ -33,17 +38,21 @@ p.setDelegate( MyDelegate() )
 listAll(p)
 
 # Setup to turn notifications on, e.g.
-svc = p.getServiceByUUID( "e198000158b346e7b9a6bedfad46833c" )
-ch = svc.getCharacteristics()[0]
+#svc = p.getServiceByUUID( "e198000158b346e7b9a6bedfad46833c" )
+#ch = svc.getCharacteristics()[0]
 
 #print(ch.valHandle)
 
-p.writeCharacteristic(ch.valHandle+1, "\x02\x00")
+#p.writeCharacteristic(ch.valHandle+1, "\x02\x00")
+#
+#while True:
+#    if p.waitForNotifications(int(timeout*60)):
+#        # handleNotification() was called
+#        continue
+#
+#    print(str(datetime.datetime.now()).split('.')[0] + ": Waited for %i min without notification..." %(timeout))
+#    # Perhaps do something else here
 
 while True:
-    if p.waitForNotifications(600):
-        # handleNotification() was called
-        continue
-
-    print("Waited for 10 min without notification...")
-    # Perhaps do something else here
+   getBatLvl(p) 
+   time.sleep(int(timeout*60))
